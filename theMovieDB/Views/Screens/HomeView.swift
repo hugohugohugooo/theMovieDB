fef72cdfc2ba48a8d2fbe9ab46971106e064cc64
Hugo Rodriguez S <hugo@Hugos-MacBook-Pro.local>
2024-08-14 12:55:23 -0600

    Testing LazyVGrid implementation

--- theMovieDB/Views/Screens/HomeView.swift
+++ theMovieDB/Views/Screens/HomeView.swift
@@ -9,14 +9,42 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var searchText: String = "Star Wars";
+    @State private var selectedTab: CGFloat = 0
+    
+    private let items = [Card(), Card(), Card(), Card(), Card(), Card()]
     
     var body: some View {
         GeometryReader { geometryReader in
             ScrollView {
                 VStack(alignment: .leading) {
+                    //Header
                     Text("What do you want to watch?")
                     SearchBar(searchText: $searchText)
-                    HCollectionView(items: [Card(), Card(), Card(), Card(), Card(), Card()], spacing: 20, itemWidth: 90, itemHeight: 160)
+                    
+                    // Popular
+                    HCollectionView(items: items, spacing: 20, itemWidth: 90, itemHeight: 160)
+                    
+                    // Categories
+                    VStack(alignment: .leading) {
+                                Picker("", selection: $selectedTab) {
+                                    Text("Now playing").tag(0)
+                                    Text("Upcoming").tag(1)
+                                    Text("Top rated").tag(2)
+                                }
+                                .pickerStyle(SegmentedPickerStyle())
+                                .padding(.bottom, 10)
+                    }
+                    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+
+                    LazyVGrid(columns: columns, spacing: 0) {
+                                    ForEach(1...6, id: \.self) { number in
+                                        Image("ForSplash")
+                                            .resizable()
+                                            .scaledToFill()
+                                            .clipped()
+                                            .cornerRadius(1.0)
+                                    }
+                                }
                 }
             }
             .scrollIndicators(/*@START_MENU_TOKEN@*/.hidden/*@END_MENU_TOKEN@*/, axes: /*@START_MENU_TOKEN@*/[.vertical, .horizontal]/*@END_MENU_TOKEN@*/)
