c849b9a741fa596ada562e7d841e9437b7bec6d2
Hugo Rodriguez S <hugo@Hugos-MacBook-Pro.local>
2024-08-13 18:29:45 -0600

    First commit. Added boilerplate folders and project configurations.

---
+++ theMovieDB/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  theMovieDB
+//
+//  Created by Hugo Rodriguez S. on 13/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ theMovieDB/theMovieDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  theMovieDBApp.swift
+//  theMovieDB
+//
+//  Created by Hugo Rodriguez S. on 13/8/24.
+//
+
+import SwiftUI
+
+@main
+struct theMovieDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
