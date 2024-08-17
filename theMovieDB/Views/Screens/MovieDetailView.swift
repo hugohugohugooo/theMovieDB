1b83d1a8f8e4574dc3ab49ecbd64992049c257c3
Hugo Rodriguez S <hugo@Hugos-MacBook-Pro.local>
2024-08-17 15:59:50 -0600

    Finished detail view ui

--- theMovieDB/Views/Screens/MovieDetailView.swift
+++ theMovieDB/Views/Screens/MovieDetailView.swift
@@ -12,74 +12,110 @@ struct MovieDetailView: View {
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                HeaderView(movie: movie)
-                GeometryReader { geometry in
-                    HStack(alignment: .top) {
-                        AsyncImage(url:movie.posterURL) { image in
-                            image.image?.resizable()
-                                .scaledToFit()
-                                .clipped()
-                                .clipShape(RoundedRectangle(cornerRadius: 25, style: .continuous))
-                                .padding(.all, 8)
-                        }
-                    }
-                    .aspectRatio(9/16, contentMode: .fit)
-                    .frame(width: geometry.size.width * 0.33)
-                    .padding(.top, geometry.size.width * 9/16 * 0.7)
-                    .padding(.leading, 32)
+            ZStack(alignment:.top) {
+                Colors.background.value
+                VStack(alignment: .leading) {
+                    HeaderView(movie: movie)
+                    DetailsView(movie: movie)
+                        .offset(y:-70)
                 }
             }
-            .background(Colors.background.value)
         }
+        .background(Colors.background.value)
+        .foregroundColor(Colors.white.value)
         .toolbar {
             ToolbarItem(placement: .principal) {
-                Text(movie.title)
+                Text("Detail")
+                    .fontWeight(.bold)
                     .foregroundColor(Colors.white.value)
             }
         }
         .toolbarBackground(Colors.background.value, for: .navigationBar)
         .toolbarBackground(.visible, for: .navigationBar)
-
+        
     }
 }
-
-//#Preview {
-//    MovieDetailView(movie: .init(id: 0, title: "Star Wars", overview: "This is Star Wars.", voteAverage: 4.5, voteCount: 900, runtime: 12000))
-//}
 
 struct HeaderView: View {
     let movie: Movie
     var body: some View {
-        GeometryReader { geometry in
-            ZStack {
-                // Header
-                AsyncImage(url: movie.backdropURL) { image in
-                    VStack {
-                        image.image?.resizable()
-                    }
+        ZStack {
+            // Header
+            AsyncImage(url: movie.backdropURL) { image in
+                VStack {
+                    image.image?.resizable()
                 }
-                Button("", systemImage: "play.circle") {
-                    // TODO: Open video
-                }
-                .foregroundColor(Colors.white.value)
-                .font(.system(size: 100))
-                .opacity(0.7)
-                
-                HStack {
-                    Spacer()
-                    VStack {
-                        Spacer()
-                        Label(movie.ratingText, systemImage: "star")
-                            .padding(10)
-                            .foregroundColor(Colors.orange.value)
-                            .background(.thinMaterial, in: .rect(cornerRadius: 8.0))
-                            .environment(\.colorScheme, .dark)
-                    }
-                }
-                .padding()
             }
-            .frame(width: geometry.size.width, height:geometry.size.width * 9/16)
+            Button("", systemImage: "play.circle") {
+                // TODO: Open video
+            }
+            .foregroundColor(Colors.white.value)
+            .font(.system(size: 100))
+            .opacity(0.7)
+            
+            ratingView(movie: movie)
         }
+        .aspectRatio(16/9, contentMode: .fit)
     }
+}
+
+struct ratingView: View {
+    let movie: Movie
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                Label(movie.ratingText, systemImage: "star")
+                    .padding(10)
+                    .foregroundColor(Colors.orange.value)
+                    .background(.thinMaterial, in: .rect(cornerRadius: 8.0))
+                    .environment(\.colorScheme, .dark)
+            }
+        }
+        .padding()
+    }
+}
+
+struct DetailsView: View {
+    let movie: Movie
+    var body: some View {
+        VStack(alignment:.leading, spacing: 15) {
+            HStack(alignment: .lastTextBaseline, spacing: 15) {
+                AsyncImage(url:movie.posterURL) { image in
+                    image.image?
+                        .resizable()
+                        .scaledToFit()
+                        .aspectRatio(9/16, contentMode:.fit)
+                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+
+                }
+                .containerRelativeFrame(.horizontal, count: 3, span: 1, spacing: 0)
+                // Title
+                Text(movie.title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .lineLimit(2)
+                    .truncationMode(.tail)
+            }
+        
+            // Details
+            HStack(alignment:.center) {
+                Label(movie.releaseDate ??
+                      "n/a", systemImage: "calendar")
+                Label(movie.durationText, systemImage: "clock")
+                Label(String(movie.id), systemImage: "ticket")
+            }
+            .foregroundColor(Color.gray)
+            
+            // Overview
+            Text(movie.overview)
+                .font(.body)
+        }
+        .padding(.horizontal, 30)
+    }
+}
+
+#Preview {
+    MovieDetailView(movie: .init(id: 0, title: "Star Wars", overview: "This is Star Wars.", voteAverage: 4.5, voteCount: 900, runtime: 12000, releaseDate: "2020"))
 }
